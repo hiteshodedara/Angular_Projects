d4b7cf1a6018c1d3e567acd903bc6c1c1ada39dc
hiteshodii <hiteshodedara1010@gamil.com>
2023-10-17 19:54:03 +0530

    all set

---
+++ practice-project/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public uid=0;
+  public uname="hitesh"
+  public pass=true;
+  public itype="text";
+
+
+
+
+
+  
+  title = 'practice-project';
+}
